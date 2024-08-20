0865270fb8677a576e2cdd53e602cc3ad0e723c9
yusufuysal <yusuf.uysal@agu.edu.tr>
2024-08-20 19:36:38 +0300

    Add new todo item via form

---
+++ README.md
@@ -0,0 +1,9 @@
+Todo List App
+Design a Todo list web application using Vanilla JavaScript. 
+The application should allow users to:
+
+Requirements:
+- Functionality: Adding, editing, and deleting tasks.
+- User Interface: Intuitive and easy to use.
+- Error Handling: Errors should me handled effectively.
+- Code Quality: Well-structured, readable, and organized.
--- todo-list-app/index.js
+++ todo-list-app/index.js
@@ -1,1 +1,40 @@
-console.log("todo app");
+// select the input element and get its value when changes and set it to a variable
+
+let newTodoName = "";
+
+const form = document.querySelector(".todo-form");
+const input = document.querySelector(".todo-input");
+const list = document.querySelector(".todo-list");
+const listItem = document.querySelector(".todo-list-item");
+
+function addNewTodoItem() {
+  const newListItem = document.createElement("li");
+
+  const itemNameSpan = document.createElement("span");
+  itemNameSpan.innerText = newTodoName;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "Edit";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+
+  newListItem.append(itemNameSpan, editBtn, deleteBtn);
+  list.append(newListItem);
+}
+
+input.addEventListener("input", function () {
+  newTodoName = this.value;
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (newTodoName !== "") {
+    // call a function to add a new item to the list
+    addNewTodoItem();
+    newTodoName = "";
+  }
+
+  console.log(`${newTodoName} is added.`);
+});
